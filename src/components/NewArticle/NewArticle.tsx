c4f7c2c8c50d4379d03c47d2bfa6a033bfbb5db4
Andrey Romashev <andreygaranin.0@yandex.ru>
2024-11-05 12:58:20 +0700

    add tags

--- src/components/NewArticle/NewArticle.tsx
+++ src/components/NewArticle/NewArticle.tsx
@@ -1,13 +1,26 @@
-import { useEffect, useState } from 'react'
+import { useState } from 'react'
+import { Button } from 'antd'
 import { v4 as uuidv4 } from 'uuid'
 
 import './new-article/new-article.sass'
 
 const NewArticle: React.FC = () => {
-  const [tags, setTags] = useState<string[]>(['text', 'lol'])
-  useEffect(() => {
-    setTags([...tags, 'foo'])
-  }, [])
+  const [newTag, setNewTag] = useState<string>('')
+  const [tags, setTags] = useState<string[]>([])
+
+  const handleAddNewTag = () => {
+    if (newTag.trim().length !== 0) {
+      if (!tags.includes(newTag)) {
+        setTags([...tags, newTag])
+      }
+      setNewTag('')
+    }
+  }
+
+  const deleteTag = (tag: string) => {
+    setTags(tags.filter((element) => element !== tag))
+  }
+
   return (
     <div className="new-article">
       <div className="new-article__container">
@@ -29,15 +42,49 @@ const NewArticle: React.FC = () => {
             Tags
             {tags.length !== 0 &&
               tags.map((tag) => (
-                <input
-                  type="text"
-                  disabled
-                  className="new-article__tag-input"
-                  placeholder="Tag"
-                  value={tag}
-                  key={uuidv4()}
-                />
+                <div className="new-article__add-tag" key={uuidv4()}>
+                  <input type="text" disabled className="new-article__tag-input" placeholder="Tag" value={tag} />
+                  <Button
+                    type="default"
+                    variant="outlined"
+                    color="danger"
+                    size="large"
+                    className="new-article__button"
+                    onClick={() => deleteTag(tag)}
+                  >
+                    Delete
+                  </Button>
+                </div>
               ))}
+            <div className="new-article__add-tag">
+              <input
+                type="text"
+                value={newTag}
+                className="new-article__tag-input"
+                placeholder="Tag"
+                onChange={(e) => setNewTag(e.target.value)}
+              />
+              <Button
+                type="default"
+                variant="outlined"
+                color="danger"
+                size="large"
+                className="new-article__button"
+                onClick={() => setNewTag('')}
+              >
+                Delete
+              </Button>
+              <Button
+                type="default"
+                variant="outlined"
+                color="primary"
+                size="large"
+                className="new-article__button"
+                onClick={() => handleAddNewTag()}
+              >
+                Add tag
+              </Button>
+            </div>
           </div>
         </form>
       </div>
--- src/types/type.ts
+++ src/types/type.ts
@@ -88,3 +88,4 @@ export interface PrivateRouteProps {
   element: ReactElement
   auth: boolean
 }
+
